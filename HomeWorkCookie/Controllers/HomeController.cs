d886b3eda857ad886793fe838448f96248aa073e
Admin <Admin@DESKTOP-QQR48TL>
2023-05-22 00:21:13 +0600

    Добавьте файлы проекта.

---
+++ HomeWorkCookie/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using HomeWorkCookie.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace HomeWorkCookie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<string> onlineUsers = new List<string>();
+
+        public IActionResult Index()
+        {
+            ViewData["OnlineUsersCount"] = onlineUsers.Count;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(string username)
+        {
+            if (!string.IsNullOrEmpty(username) && !onlineUsers.Contains(username))
+            {
+                onlineUsers.Add(username);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult RemoveUser(string username)
+        {
+            if (!string.IsNullOrEmpty(username) && onlineUsers.Contains(username))
+            {
+                onlineUsers.Remove(username);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
